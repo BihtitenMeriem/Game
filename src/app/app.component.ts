3d8660543996cde8d84eadef1ed9cad6476f6c54
MaryBih <marybih41@gmail.com>
2024-01-14 20:25:04 +0100

    v1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-
+import { HomeComponent } from './home/home.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
+  imports: [CommonModule, RouterOutlet ,HomeComponent],
+  template: `
+  <main>
+    <section>
+      <app-home></app-home>
+    </section>
+  </main>
+  `,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
